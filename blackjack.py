b91e25c4b58065ab5f690ca9ac7be2558ed8e4f9
dousetabs <dousetabs@gmail.com>
2025-04-05 00:45:59 +0800

    buxixi

--- blackjack.py
+++ blackjack.py
@@ -952,7 +952,7 @@ class BlackJack(Plugin):
                     current_losses = int(player.total_losses)
                     player.total_losses = str(current_losses + 1)
                     if player.god:
-                        player.data["chips"] += 1000
+                        player.data["chips"] = str(1000 + player.chips)
                     
                     result.append(f"{player.nickname} {hand_marker}: {player_value}点 < 庄家{dealer_value}点，输掉 {bet_amount} 筹码")
                     
